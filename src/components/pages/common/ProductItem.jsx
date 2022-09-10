e3380a74154b46a8c4ac68cce48ccc58206bb134
Narmin <mnermin064@gmail.com>
2022-09-10 21:03:19 +0400

    New commit

--- src/components/pages/common/ProductItem.jsx
+++ src/components/pages/common/ProductItem.jsx
@@ -1,23 +1,30 @@
-import React from 'react'
-import { useCart } from 'react-use-cart'
-import { useTranslation } from 'react-i18next'
+import React from "react";
+import { useCart } from "react-use-cart";
+import { useTranslation } from "react-i18next";
+import { toast } from "react-toastify";
 const ProductItem = (props) => {
-    const { addItem } = useCart();
-    const { t } = useTranslation();
-    return (
-            <div className="box" key={props.id}
-            >
-                <div className="box-img">
-                    <img src={props.img} alt=''/>
-                    <span>Fresh</span>
-                </div>
-                <div className="box-content">
-                    <h4>{t(props.title)}</h4>
-                    <h6>{props.price}$</h6>
-                    <button onClick={()=>addItem(props.product)}>Add to card</button>
-                </div>
-            </div>
-    )
-}
+  const { addItem } = useCart();
+  const { t } = useTranslation();
+  return (
+    <div className="box" key={props.id}>
+      <div className="box-img">
+        <img src={props.img} alt="" />
+        <span>Fresh</span>
+      </div>
+      <div className="box-content">
+        <h4>{t(props.title)}</h4>
+        <h6>{props.price}$</h6>
+        <button
+          onClick={() => {
+            addItem(props.product);
+            toast('yes')
+          }}
+        >
+          Add to card
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default ProductItem
+export default ProductItem;
--- src/components/router/AppRouter.jsx
+++ src/components/router/AppRouter.jsx
@@ -1,36 +1,40 @@
-import React from 'react'
-import { BrowserRouter, Route,Routes } from 'react-router-dom'
-import Nav from "../pages/common/Nav"
-import Footer from "../pages/common/Footer"
-import Home from "../pages/Home"
-import About from '../pages/About'
-import Contact from "../pages/Contact"
-import Register from '../pages/Register'
-import Cart from '../pages/Cart'
-import Signin from "../pages/Signin"
-import Blogs from '../pages/Blogs'
-import Shop from '../pages/Shop'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "../pages/common/Nav";
+import Footer from "../pages/common/Footer";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Register from "../pages/Register";
+import Cart from "../pages/Cart";
+import Signin from "../pages/Signin";
+import Blogs from "../pages/Blogs";
+import Shop from "../pages/Shop";
 import { CartProvider } from "react-use-cart";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.min.css';
+import {  Zoom } from 'react-toastify';
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
-      <Nav />
-        <CartProvider>
-         <Routes>
-         <Route  path='/' element={<Home/>}></Route>
-          <Route path='/about' element={<About/>}></Route>
-          <Route path='/contact' element={<Contact/>}></Route>
-          <Route path='/register' element={<Register/>}></Route>
-          <Route path='/signin' element={<Signin/>}></Route>
-          <Route path='/cart' element={<Cart/>}></Route>
-          <Route path='/shop' element={<Shop/>}></Route>
-          <Route path='/blogs' element={<Blogs/>}></Route>
-         </Routes>
-        </CartProvider>
+      <CartProvider>
+        <ToastContainer pauseOnHover={false} autoClose={1000} transition={Zoom}/>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/signin" element={<Signin />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/shop" element={<Shop />}></Route>
+          <Route path="/blogs" element={<Blogs />}></Route>
+        </Routes>
+      </CartProvider>
       <Footer />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
