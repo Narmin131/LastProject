ac42c48032b790a404e45b993af4f89fe4ced8a9
Narmin <mnermin064@gmail.com>
2022-11-24 23:52:47 +0400

    New commit

--- src/components/store/Store.jsx
+++ src/components/store/Store.jsx
@@ -1,9 +1,16 @@
-import { combineReducers, createStore} from "redux";
-import wishlist from "../store/reducer/AppReducer"
+import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, createStore } from "redux";
+import wishlist from "../store/reducer/AppReducer";
 
-const store = createStore(
-    combineReducers({
-      wishlist
-    })
-)
+// const store = createStore(
+//     combineReducers({
+//       wishlist
+//     })
+// )
+
+const store = configureStore({
+  reducer: {
+    wishlist,
+  },
+});
 export default store;
